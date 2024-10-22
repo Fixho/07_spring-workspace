ccdcf30ae7bba37dcee117a9ea8325053d9f62f4
fixho <rhwjdgh12@naver.com>
2024-10-22 15:48:37 +0900

    spring+mybatis 합쳐서 진행함

--- 04_Spring_MyBatis_AOP/src/main/java/com/br/sbatis/controller/NoticeController.java
+++ 04_Spring_MyBatis_AOP/src/main/java/com/br/sbatis/controller/NoticeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.br.sbatis.dto.NoticeDto;
@@ -30,6 +31,62 @@ public class NoticeController {
 		//return "notice/list";
 	}
 	
+	// a태그는 대부분 get맵핑으로 사용
+	@GetMapping("/detail.do")   //  /notice/detail.do
+	public void noticeDetail(int no, Model model) {
+		NoticeDto n = noticeService.selectNoticeByNo(no);
+		model.addAttribute("n", n);
+	}
 	
+	@GetMapping("/enroll.do") //   /notice/enroll.do
+	public void noticeEnroll() {
+		
+	}
+	
+	@PostMapping("/insert.do")
+	public String noticeInsert(NoticeDto n) {
+		int result = noticeService.insertNotice(n);
+		if(result > 0) { // 성공 => 리스트페이지
+			return "redirect:/notice/list.do";
+		}else { // 실패 => 메인페이지
+			return "redirect:/";
+		}
 
+	}
+	
+	@GetMapping("/modify.do")	//		/notice/modify.do
+	public void noticeModify(int no, Model model) {
+		model.addAttribute("n", noticeService.selectNoticeByNo(no));
+	}
+	
+	@PostMapping("/update.do")
+	public String noticeUpdate(NoticeDto n) {
+		int result = noticeService.updateNotice(n);
+		if(result > 0) { // 성공 => 상세페이지
+			return "redirect:/notice/detail.do?no=" + n.getNo();
+		}else { // 실패 => 목록페이지
+			return "redirect:/notice/list.do";
+		}
+	}
+	
+	@PostMapping("/delete.do")
+	public String noticeDelete(String[] deleteNo) {
+		//String[] arr = request.getParameterValues("deleteNo") jsp때한거
+		// delete from notice where no in (xx, xx, xx) 쿼리가 실행될수 있도록
+		// service 메소드 수정, dao 메소드 만들기, mapper에 쿼리작성
+		
+		// 성공했을경우 -> 목록페이지
+		// 실패했을경우 -> 메인페이지
+		int result = noticeService.deleteNotice(deleteNo);
+		if(result == deleteNo.length) {
+			return "redirect:/notice/list.do";
+		}else {
+			return "redirect:/";
+		}
+		
+		
+	}
+	
+	
+	
 }
--- 04_Spring_MyBatis_AOP/src/main/java/com/br/sbatis/dao/NoticeDao.java
+++ 04_Spring_MyBatis_AOP/src/main/java/com/br/sbatis/dao/NoticeDao.java
@@ -15,8 +15,27 @@ public class NoticeDao {
 	
 	private final SqlSessionTemplate sqlSession;
 	
+	// 리스트 조회시 selectList 사용
 	public List<NoticeDto> selectNoticeList(){
 		return sqlSession.selectList("noticeMapper.selectNoticeList");
+	}
+	
+	// 리스트 하나만 조회시 selectOne 사용 하고 SQL(맵퍼에서) 완성시킬 무언가를 사용해야되니깐 noticeNo 필요함
+	public NoticeDto selectNoticeByNo(int noticeNo) {
+		return sqlSession.selectOne("noticeMapper.selectNoticeByNo", noticeNo);
+	}
+	
+	// return sqlSession 이후에 sql문에서 실행할 명령문 작성
+	public int insertNotice(NoticeDto n) {
+		return sqlSession.insert("noticeMapper.insertNotice", n);
+	}
+	
+	public int updateNotice(NoticeDto n) {
+		return sqlSession.update("noticeMapper.updateNotice", n);
+	}
+	
+	public int deleteNotice(String[] deleteNo) {
+		return sqlSession.delete("noticeMapper.deleteNotice", deleteNo);
 	}
 	
 	
